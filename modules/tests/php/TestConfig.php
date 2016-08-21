fc9b451da2fb5326c84a697008836748f487d803
Cambell <cambell.prince@gmail.com>
2016-08-21 11:05:31 +0700

    tests: Add conditional around path defines

--- modules/tests/php/TestConfig.php
+++ modules/tests/php/TestConfig.php
@@ -2,8 +2,14 @@
 
 ini_set('xdebug.show_exception_trace', 0);
 
-$rootPath = realpath(__DIR__ . '/../../..');
-define('ROOT_PATH', $rootPath);
-define('SRC_PATH', $rootPath);
-define('TEST_PATH', $rootPath . '/modules/tests/php');
+if (!defined('ROOT_PATH')) {
+	$rootPath = realpath(__DIR__ . '/../../..');
+	define('ROOT_PATH', $rootPath);
+}
+if (!defined('SRC_PATH')) {
+	define('SRC_PATH', $rootPath);
+}
+if (!defined('TEST_PATH')) {
+	define('TEST_PATH', $rootPath . '/modules/tests/php');
+}
 
